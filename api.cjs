fd98b0f88c81a3a074ef6ab406498704d595a488
Aaron <metalhead5209@gmail.com>
2023-03-14 07:42:45 -0500

    added gpt chat completion functionality

--- api.cjs
+++ api.cjs
@@ -1,7 +1,18 @@
 const express = require('express');
 const handleBars = require('express-handlebars');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const { Configuration, OpenAIApi } = require('openai');
 const app = express();
+
+// GPT Configuration
+const KEY = process.env.API_KEY;
+const ORG = process.env.ORG
+const gptconfig = new Configuration({
+    apiKey: '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',
+    organization: ORG
+});
+const openAiConfig = new OpenAIApi(gptconfig);
 
 
 // View Engine
@@ -12,12 +23,27 @@ app.set('view engine', 'hbs');
 // Middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 
-
+// Routes
 app.get('/', (req, res) => {
     res.render('index')
 })
+
+app.post('/', async (req, res) => {
+    const q = req.body.q;
+    const completion = await openAiConfig.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: q}]
+    });
+    const message = completion.data.choices[0].message;
+    console.log(message);
+    res.render('index', { message })
+})
+
+
+
 
 
 app.listen(3001, () => {
