dc7a7e64eafdc67abe333f2e970f6e106c97e203
Aaron <metalhead5209@gmail.com>
2023-03-14 14:45:56 -0500

    started api logic for SERPapi, added packages and set up POST route

--- api.cjs
+++ api.cjs
@@ -2,29 +2,35 @@ const express = require('express');
 const handleBars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
-const { Configuration, OpenAIApi } = require('openai');
 const app = express();
 
+
+
 // GPT Configuration
-const KEY = process.env.API_KEY;
+const { Configuration, OpenAIApi } = require('openai');
+const gptKEY = process.env.API_KEY;
 const ORG = process.env.ORG
 const gptconfig = new Configuration({
-    apiKey: KEY,
+    apiKey: gptKEY,
     organization: ORG
 });
 const openAiConfig = new OpenAIApi(gptconfig);
 
 
+// SERPApi Configuration
+const SerpApi = require('google-search-results-nodejs');
+const search = new SerpApi.GoogleSearch(key);
+const imgDB = [];
 
 // View Engine
-app.engine('hbs', handleBars.engine({ extname: ".hbs"}));
+app.engine('hbs', handleBars.engine({ extname: ".hbs" }));
 app.set('view engine', 'hbs');
 
 
 // Middleware
 app.use(express.static('public'));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 // Routes
@@ -36,12 +42,36 @@ app.post('/', async (req, res) => {
     const q = req.body.q;
     const completion = await openAiConfig.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: q}]
+        messages: [{ role: "user", content: q }]
     });
     const message = completion.data.choices[0].message;
     console.log(message.content)
     res.render('index', { message });
 
+});
+
+app.post('/api', (req, res) => {
+    const params = {
+        q: `${req.body.results[0].name} toy`,
+        tbm: "isch",
+        ijn: 0,
+        safe: "active",
+        star: 1,
+        num: 1,
+    };
+    console.lof(req.body.results[0].name)
+    const callback = (data) => {
+        const IMAGE = {
+            id: Math.floor(Math.random() * 200) + 1,
+            url: data["images_results"][Math.floor(Math.random() * 20)]["original"],
+        };
+        console.log(IMAGE);
+        imgDB.push(IMAGE);
+    };
+    search.json(params, callback);
+    res.json({
+        message: "Hello There",
+    });
 })
 
 
